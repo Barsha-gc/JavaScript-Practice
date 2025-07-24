c8f7194331c0dc53cb05304b55c0f4774c930efa
Barsha G.C. <barshagc2005@gmail.com>
2025-07-24 17:38:58 +0545

    Remove Q1.js from the repository

--- Q3.js
+++
@@ -1,7 +0,0 @@
-// Reverse a number. (e.g: 123->321)
-
-function reverseNumber(num) {
-  const reversed = num.toString().split("").reverse().join("");
-  return parseInt(reversed);
-}
-console.log(reverseNumber(764));
--- Q5.js
+++
@@ -1,7 +0,0 @@
-function findMinMax(arr) {
-  const min = Math.min(...arr);
-  const max = Math.max(...arr);
-  console.log(`Minimum: ${min}`);
-  console.log(`Maximum: ${max}`);
-}
-findMinMax([100, 44, 888, 234, 322, 0]);
